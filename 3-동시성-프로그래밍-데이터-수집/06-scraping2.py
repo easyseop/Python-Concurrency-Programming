302d0f5fbfc7987afdfd9e28a1a432c01eac957a
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-03-13 18:28:02 +0900

    0313

--- 3-동시성-프로그래밍-데이터-수집/06-scraping2.py
+++ 3-동시성-프로그래밍-데이터-수집/06-scraping2.py
@@ -1,6 +1,23 @@
 import aiohttp
 import asyncio
+import aiofiles
+import os
 from config import get_secret
+
+
+async def img_downloader(session, img):
+    img_name = img.split("/")[-1].split("?")[0]
+
+    try:
+        os.mkdir("./images")
+    except FileExistsError:
+        pass
+
+    async with session.get(img) as response:
+        if response.status == 200:
+            async with aiofiles.open(f"./images/{img_name}", mode="wb") as file:
+                img_data = await response.read()
+                await file.write(img_data)
 
 
 async def fetch(session, url, i):
@@ -16,6 +33,9 @@ async def fetch(session, url, i):
         items = result["items"]
         images = [item["link"] for item in items]
         print(images)
+
+    async with aiohttp.ClientSession() as session:
+        await asyncio.gather(*[img_downloader(session, img) for img in images])
 
 
 async def main():
